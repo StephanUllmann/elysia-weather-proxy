5c68fa9f1910705e3dcc8d3c46736f8311f55abb
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-04-30 10:07:19 +0200

    weather query

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import { Elysia } from "elysia";
+import { Elysia, Handler } from 'elysia';
+const weatherApiKey = process.env.WEATHER_API_KEY;
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+if (!weatherApiKey) {
+  console.log('No API key');
+  process.exit(1);
+}
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const weatherHandler: Handler = async ({ request, server, query }) => {
+  const { lat, lan } = query;
+  let data;
+  try {
+    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${lat},${lan}`);
+    data = await res.json();
+  } catch (error) {
+    console.log(error);
+    return { message: 'Lookup failed' };
+  }
+
+  return {
+    now: 'Hey',
+    ip: server?.requestIP(request),
+    data,
+  };
+};
+
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .get('/weather-now', weatherHandler)
+  .listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
