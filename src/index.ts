93c3aa379e6e66b5a4e2eea2d6e1b00f03e65ff7
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-05-02 14:14:31 +0200

    render

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Elysia, Handler } from 'elysia';
+import { cors } from '@elysiajs/cors';
 const weatherApiKey = process.env.WEATHER_API_KEY;
 const port = process.env.PORT ?? 3000;
 
@@ -7,15 +8,21 @@ if (!weatherApiKey) {
   process.exit(1);
 }
 
+let data;
+
 const weatherHandler: Handler = async ({ request, server, query }) => {
-  const { lat, lan } = query;
-  let data;
-  try {
-    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${lat},${lan}`);
-    data = await res.json();
-  } catch (error) {
-    console.log(error);
-    return { message: 'Lookup failed' };
+  const { lat, lon } = query;
+  if (!data) {
+    console.log('Running');
+    try {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${lat},${lon}&hour_fields=temp_c&days=2`
+      );
+      data = await res.json();
+    } catch (error) {
+      console.log(error);
+      return { message: 'Lookup failed' };
+    }
   }
 
   return {
@@ -26,6 +33,7 @@ const weatherHandler: Handler = async ({ request, server, query }) => {
 };
 
 const app = new Elysia()
+  .use(cors())
   .get('/', () => 'Hello Elysia')
   .get('/weather-now', weatherHandler)
   .listen(port);
